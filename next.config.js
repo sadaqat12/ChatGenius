df09eafbe88004f6ea7023071e98b7ffde125763
Sadaqat Ali <sadaqat12@github.com>
2025-01-12 23:50:26 -0500

    removing duplicates for vercel deployment

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ],
   },
+  typescript: {
+    // Comment this out if you want to catch type errors during build
+    // ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Comment this out if you want to catch lint errors during build
+    // ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
